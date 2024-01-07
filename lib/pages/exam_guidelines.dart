6d962be3f46727ece4c9303439ea73b10c3951d8
Great Grant <greatgrant116@gmail.com>
2024-01-07 11:04:39 +0100

    refactor and remove redundant code in exam guidelines

--- lib/pages/exam_guidelines.dart
+++ lib/pages/exam_guidelines.dart
@@ -43,11 +43,11 @@ class ExamGuidelinesScreen extends StatelessWidget {
             buildParagraph(
                 "The Faculty of Engineering drawing room is well equipped with drawing tables and chairs for the students while doing their drawing assignment and also when defending the assignment(s). At the end of the semester, all the assignments given are used as their continuous assessment, which takes 50% while examination takes 50%."),
             buildHeading("Exam Regulations"),
-            buildOrderedListItem(
+            buildParagraph(
                 "A student shall be at the examination room at least ten (10) minutes before the advertised time of the examination. A student is required to supply his/her own pens, pencils, rulers, calculators, etc. No one shall be permitted to borrow any material from another student during the course of the examination."),
-            buildOrderedListItem(
+            buildParagraph(
                 "A student may be admitted up to thirty minutes after the start of the examination but he/she shall not be allowed extra time. If a student arrives later than thirty minutes after the commencement of the extermination, an invigilator may at his discretion, admit him if he/she is satisfied that the student has good reasons for his/her lateness. The invigilator shall report the circumstances to the faculty examination officers who shall advise the board of examiners, which shall decide whether to accept the student paper."),
-            buildOrderedListItem(
+            buildParagraph(
                 "A student may be permitted by an invigilator to leave the examination room in the course of an examination provided that:"),
             buildUnorderedListItem(
                 "He/she doesn’t leave during the first hour or the last fifteen (15) minutes of the examination."),
@@ -55,52 +55,52 @@ class ExamGuidelinesScreen extends StatelessWidget {
                 "He/she hands his script to the invigilator before leaving, if he/she doesn’t intend to return."),
             buildUnorderedListItem(
                 "If he/she intends to return, his script should be giving to the invigilator or any other person as directed by the invigilator throughout the period of his absence."),
-            buildOrderedListItem(
+            buildParagraph(
                 "A student brings his examination card to each examination and displays it in a prominent position on his desk."),
-            buildOrderedListItem(
+            buildParagraph(
                 "Each student shall complete the attendance register and slip which shall be collected by the invigilator of each examination."),
-            buildOrderedListItem(
+            buildParagraph(
                 "During an examination, no student shall speak to any other student or to the invigilator of each examination."),
-            buildOrderedListItem(
+            buildParagraph(
                 "Any student caught making noise or causing disturbance during an examination shall be disqualified from the whole of the semester examinations."),
-            buildOrderedListItem(
+            buildParagraph(
                 "No book, printed-paper or written document or unauthorized aid may be taken into an examination room by students, excepts as may be stated in the rubrics of any examination paper."),
-            buildOrderedListItem(
+            buildParagraph(
                 "Students are required to deposit any hand-bag, briefcase etc. outside the examination room or any other space provided for its purpose ten (10) minutes before the start of an examination."),
-            buildOrderedListItem(
+            buildParagraph(
                 "A student must not, during an examination, directly or indirectly, give assistance to any other student or permit any other student to copy from or use of his papers. Similarly, a student must not directly accept assistance from any other student or use any other student’s paper. Receiving and giving assistance during an examination carry equal punishment."),
-            buildOrderedListItem(
+            buildParagraph(
                 "If any student is found to be or suspected of cheating or disturbing the conduct of the examination in any way, a report shall be made as soon as possible to circumstance to be investigated and reported to the board of examiners provided he/she causes no disturbance but the board of examiners may subsequently recommend to the faculty board and senate whether his/her paper should be accepted and as to any other action that should be taken in the case."),
-            buildOrderedListItem(
+            buildParagraph(
                 "A student should write his/her registration number, not his name distinctly at the top of the cover of every answer booklet or separate sheet of paper."),
-            buildOrderedListItem(
+            buildParagraph(
                 "The use of scrap paper is not permitted. All rough work must be done in the answer booklets which must be submitted to the invigilator. Apart from the printed question paper, a student may not remove from the examination room or mutilate any paper or other material supplied."),
-            buildOrderedListItem(
+            buildParagraph(
                 "At the end of the time allotted, each student shall stop writing when instructed to do so and shall gather his scripts together ready for collection by the invigilator."),
             buildHeading("Some specific examination misconduct and penalties applied when they are committed as approved by SENATE are given below."),
-            buildOrderedListItem("Offence: Causing commotion, rudeness, disobedience of instruction from either an examination officer or invigilator."),
+            buildParagraph("Offence: Causing commotion, rudeness, disobedience of instruction from either an examination officer or invigilator."),
             buildParagraph("Penalty: Letter of warning to the student and apology from the student to the offended officer or rustication of a semester depending on the magnitude of the offence.", isPenalty: true),
-            buildOrderedListItem("Offence: Disturbance and talking during examination."),
+            buildParagraph("Offence: Disturbance and talking during examination."),
             buildParagraph("Penalty: Rustication for one semester.", isPenalty: true),
-            buildOrderedListItem("Offence: Writing rough work on a question paper or any other material other than the examination answer script provided."),
+            buildParagraph("Offence: Writing rough work on a question paper or any other material other than the examination answer script provided."),
             buildParagraph("Penalty: Rustication for one semester.", isPenalty: true),
-            buildOrderedListItem("Offence: Taking into and/or using foreign material in the examination hall."),
+            buildParagraph("Offence: Taking into and/or using foreign material in the examination hall."),
             buildParagraph("Penalty: Expulsion from the University.", isPenalty: true),
-            buildOrderedListItem("Offence: Helping each other during examination."),
+            buildParagraph("Offence: Helping each other during examination."),
             buildParagraph("Penalty: Expulsion from the University.", isPenalty: true),
-            buildOrderedListItem("Offence: Verbal or physical assault on the examination officer, invigilator or fellow student (s)."),
+            buildParagraph("Offence: Verbal or physical assault on the examination officer, invigilator or fellow student (s)."),
             buildParagraph("Penalty: Rustication for one semester, academic session or expulsion from the University depending on the magnitude of the offence.", isPenalty: true),
-            buildOrderedListItem("Offence: Unauthorized use of programmable calculator and exchange of such calculators."),
+            buildParagraph("Offence: Unauthorized use of programmable calculator and exchange of such calculators."),
             buildParagraph("Penalty: Expulsion from the University.", isPenalty: true),
-            buildOrderedListItem("Offence: Leaving the examination hall with an answer booklet."),
+            buildParagraph("Offence: Leaving the examination hall with an answer booklet."),
             buildParagraph("Penalty: Expulsion from the University.", isPenalty: true),
-            buildOrderedListItem("Offence: Entrance of examination hall with GSM handset."),
+            buildParagraph("Offence: Entrance of examination hall with GSM handset."),
             buildParagraph("Penalty: Rustication for one semester.", isPenalty: true),
-            buildOrderedListItem("Offence: Use of GSM handset in the examination hall."),
+            buildParagraph("Offence: Use of GSM handset in the examination hall."),
             buildParagraph("Penalty: Expulsion from the University.", isPenalty: true),
-            buildOrderedListItem("Offence: Involvement of both staff and student in the leakage of any examination material."),
+            buildParagraph("Offence: Involvement of both staff and student in the leakage of any examination material."),
             buildParagraph("Penalty: Expulsion of the student (s) from the University and reporting the staff to the University administration for further action.", isPenalty: true),
-            buildOrderedListItem("Offence: Stealing of examination material such as question paper, answer booklet, etc."),
+            buildParagraph("Offence: Stealing of examination material such as question paper, answer booklet, etc."),
             buildParagraph("Penalty: Expulsion from the University and handing over student to the police for further disciplinary measures.", isPenalty: true),
             buildParagraph("Other acts of examination misconduct not mentioned above would be determined by the examination misconduct committee and final decision would be taken by the University SENATE."),
           ],
@@ -123,32 +123,9 @@ Widget buildHeading(String text) {
 Widget buildParagraph(String text, {bool isPenalty = false}) {
   return Padding(
     padding: const EdgeInsets.only(bottom: 8.0),
-    child: Text(
-      text,
-      style: TextStyle(
-        fontSize: 16,
-        fontFamily: "montserrat",
-        color: isPenalty ? Colors.red : Colors.black,
-      ),
-    ),
-  );
-}
-
-Widget buildOrderedListItem(String text, {bool isPenalty = false}) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 16.0, bottom: 8.0),
     child: Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          '•',
-          style: TextStyle(
-            fontSize: 16,
-            fontFamily: "montserrat",
-            color: isPenalty ? Colors.red : Colors.black,
-          ),
-        ),
-        const SizedBox(width: 8),
         Expanded(
           child: Text(
             text,
