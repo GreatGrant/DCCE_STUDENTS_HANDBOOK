e649eaf19aeccfc6d3ab372208dd6eb05732a9bc
Great Grant <greatgrant116@gmail.com>
2024-01-07 08:00:49 +0100

    add path find path_finder dependency

--- lib/pages/grading.dart
+++ lib/pages/grading.dart
@@ -1,7 +1,6 @@
 import 'package:dcce_handbook/util/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-
 import '../widgets/build_drawer.dart';
 import '../widgets/common_app_bar.dart';
 
@@ -20,30 +19,25 @@ class GradingScreenState extends State<GradingScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController();
+    _controller = WebViewController()..loadRequest(
+    Uri.parse('https://flutter.dev'),
+    );
     _controller.loadFlutterAsset("assets/tables/grading_table.html");
   }
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: const CommonAppBar(title: "Grading"),
       drawer: buildDrawer(context),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Text(
-              AppStrings.gradingParagraphString,
-              style: TextStyle(fontSize: 16),
-            ),
-            WebViewWidget(controller: _controller)
-
-          ],
-        ),
+      body: ListView(
+        children: [
+          const Text(
+            AppStrings.gradingParagraphString,
+            style: TextStyle(fontSize: 16),
+          ),
+          WebViewWidget(controller: _controller),
+        ],
       ),
     );
   }
-
-
 }
