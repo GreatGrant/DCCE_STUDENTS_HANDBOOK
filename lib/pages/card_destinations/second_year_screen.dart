fcad8fecefa18f82292834c21a0748baf7313a0c
Great Grant <greatgrant116@gmail.com>
2024-01-12 15:49:29 +0100

    fix tab controller bug in two hundred level screen

--- lib/pages/card_destinations/second_year_screen.dart
+++ lib/pages/card_destinations/second_year_screen.dart
@@ -1,25 +1,58 @@
 import 'package:dcce_handbook/pages/card_destinations/second_year_first_semester_screen.dart';
 import 'package:dcce_handbook/pages/card_destinations/second_year_second_semester_screen.dart';
-import 'package:flutter/cupertino.dart';
-import '../../widgets/common_tab_text.dart';
-import '../../widgets/common_year_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:dcce_handbook/widgets/common_app_bar.dart';
 
-class SecondYearScreen extends StatelessWidget {
+class SecondYearScreen extends StatefulWidget {
   const SecondYearScreen({super.key, required this.title});
+
   final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return CommonYearScreen(
-        title: title,
-        tabs: const [
-          CommonTabText(text: "First Semester"),
-          CommonTabText(text: "Second Semester"),
-        ],
-        tabViewChildren: const [
-          SecondYearFirstSemester(title: "Second Year First Semester"),
-          SecondYearSecondSemester(title: "Second Year Second Semester"),
-        ]);
-  }
+  SecondYearScreenState createState() => SecondYearScreenState();
 }
 
+class SecondYearScreenState extends State<SecondYearScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: _getTabs().length,
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CommonAppBar(
+          title: widget.title,
+          tabs: _getTabs(),
+          tabController: _tabController
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          SecondYearFirstSemester(title: widget.title),
+          SecondYearSecondSemester(title: widget.title),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _getTabs() {
+    return [
+      const Tab(text: "First Semester"),
+      const Tab(text: "Second Semester"),
+    ];
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+}
