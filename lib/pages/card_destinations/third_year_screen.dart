11e2770b4d5d1d568d1483b92661ad471089edb8
Great Grant <greatgrant116@gmail.com>
2024-01-13 23:44:02 +0100

    bug fix in third year screen

--- lib/pages/card_destinations/third_year_screen.dart
+++ lib/pages/card_destinations/third_year_screen.dart
@@ -1,24 +1,58 @@
 import 'package:dcce_handbook/pages/card_destinations/third_year_first_semester_screen.dart';
 import 'package:dcce_handbook/pages/card_destinations/third_year_second_semester_screen.dart';
-import 'package:flutter/cupertino.dart';
-import '../../widgets/common_tab_text.dart';
-import '../../widgets/common_year_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:dcce_handbook/widgets/common_app_bar.dart';
 
-class ThirdYearScreen extends StatelessWidget {
+class ThirdYearScreen extends StatefulWidget {
   const ThirdYearScreen({super.key, required this.title});
+
   final String title;
 
   @override
+  ThirdYearScreenState createState() => ThirdYearScreenState();
+}
+
+class ThirdYearScreenState extends State<ThirdYearScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: _getTabs().length,
+      vsync: this,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CommonYearScreen(
-        title: title,
-        tabs: const [
-          CommonTabText(text: "First Semester"),
-          CommonTabText(text: "Second Semester"),
+    return Scaffold(
+      appBar: CommonAppBar(
+          title: widget.title,
+          tabs: _getTabs(),
+          tabController: _tabController
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ThirdYearFirstSemester(title: widget.title),
+          ThirdYearSecondSemester(title: widget.title),
         ],
-        tabViewChildren: const [
-          ThirdYearFirstSemester(title: "Third Year First Semester"),
-          ThirdYearSecondSemester(title: "Third Year Second Semester"),
-        ]);
+      ),
+    );
+  }
+
+  List<Widget> _getTabs() {
+    return [
+      const Tab(text: "First Semester"),
+      const Tab(text: "Second Semester"),
+    ];
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
