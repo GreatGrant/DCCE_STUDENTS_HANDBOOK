000041651e18de61bf0fcc07e4325befffec19ca
Great Grant <greatgrant116@gmail.com>
2024-01-14 16:15:16 +0100

    bug fix in final year screen

--- lib/pages/card_destinations/final_year_screen.dart
+++ lib/pages/card_destinations/final_year_screen.dart
@@ -1,24 +1,61 @@
-import 'package:flutter/cupertino.dart';
-import '../../widgets/common_tab_text.dart';
-import '../../widgets/common_year_screen.dart';
+import 'package:dcce_handbook/pages/card_destinations/second_year_first_semester_screen.dart';
+import 'package:dcce_handbook/pages/card_destinations/second_year_second_semester_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:dcce_handbook/widgets/common_app_bar.dart';
+
 import 'final_year_first_semester_screen.dart';
 import 'final_year_second_semester_screen.dart';
 
-class FinalYearScreen extends StatelessWidget {
+class FinalYearScreen extends StatefulWidget {
   const FinalYearScreen({super.key, required this.title});
+
   final String title;
 
   @override
+  FinalYearScreenState createState() => FinalYearScreenState();
+}
+
+class FinalYearScreenState extends State<FinalYearScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: _getTabs().length,
+      vsync: this,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CommonYearScreen(
-        title: title,
-        tabs: const [
-          CommonTabText(text: "First Semester"),
-          CommonTabText(text: "Second Semester"),
+    return Scaffold(
+      appBar: CommonAppBar(
+          title: widget.title,
+          tabs: _getTabs(),
+          tabController: _tabController
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          FinalYearFirstSemester(title: widget.title),
+          FinalYearSecondSemester(title: widget.title),
         ],
-        tabViewChildren: const [
-          FinalYearFirstSemester(title: "Second Year First Semester"),
-          FinalYearSecondSemester(title: "Second Year Second Semester"),
-        ]);
+      ),
+    );
+  }
+
+  List<Widget> _getTabs() {
+    return [
+      const Tab(text: "First Semester"),
+      const Tab(text: "Second Semester"),
+    ];
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
