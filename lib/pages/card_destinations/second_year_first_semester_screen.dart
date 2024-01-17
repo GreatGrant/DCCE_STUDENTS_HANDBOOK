13f6666422079e694002ee8cb7a938750c2d7290
Great Grant <greatgrant116@gmail.com>
2024-01-17 14:36:18 +0100

    populate three hundred fs

--- lib/pages/card_destinations/second_year_first_semester_screen.dart
+++ lib/pages/card_destinations/second_year_first_semester_screen.dart
@@ -27,109 +27,109 @@ class SecondYearFirstSemesterState extends State<SecondYearFirstSemester>{
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-            padding: EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(8.0),
             child: SingleChildScrollView(
                 child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CourseHeader('CE 211: STRENGTH OF MATERIALS I (2 UNITS)'),
-                CourseContent(
+                const CourseHeader('CE 211: STRENGTH OF MATERIALS I (2 UNITS)'),
+                const CourseContent(
                   content:
                       'Introduction to stress and strain; some simple states of stress and strain; stresses; relationship between loading, shearing forces and bending moment, bending stresses; combined bending and direct stress torsional loading; composite shafts and torsional strain energy.',
                 ),
-                CourseHeader(
+                const CourseHeader(
                     'EE 211: ELECTRICAL ENGINEERING FUNDAMENTALS I (3 UNITS)'),
-                CourseContent(
+                const CourseContent(
                   content:
                       'Introduction to basic electrical and electronics engineering concepts.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title: 'Circuit Theory:',
                   content:
                       'Elements, DC and AC circuits, symbolic notations, resonance and resonant circuits, circuit laws and theorems; fundamental and harmonics; 3-phase circuit theory, power and power measurements. Power factor; transfer function concepts, RC, RL and RCL circuits, Field theory, Electromagnetism and magnetic circuits applications, capacitors, basic electromagnetic laws and theorems. Measurement circuits.',
                 ),
-                CourseHeader('ME 211: ENGINEERING MECHANICS I (2 UNITS)'),
-                CourseContent(
+                const CourseHeader('ME 211: ENGINEERING MECHANICS I (2 UNITS)'),
+                const CourseContent(
                   content:
                       'Fundamental laws and terminology; force systems; equilibrium of a particle and rigid body; free-body diagrams; distributed forces; virtual work and energy methods.',
                 ),
-                CourseContent(
+                const CourseContent(
                   content:
                       'Structures: trusses, frames and simple machines; kinematics and kinetics of a particle, kinematics of plane rigid bodies; examples of translation and rotation, impact.',
                 ),
-                CourseHeader('ME 212: ENGINEERING DRAWING I (2 UNITS)'),
-                CourseContent(
+                const CourseHeader('ME 212: ENGINEERING DRAWING I (2 UNITS)'),
+                const CourseContent(
                   content:
                       'Conic section (Ellipse, Parabola and |Hyperbola). Orthographic projections (Third Angle). Loci (Cycloid and involute). Loci of mechanics. Civil Engineering Drawing: Types of buildings, axonometric views, house and roof styles, doors and windows, staircases, Exercise in blueprint reading and drawing.',
                 ),
-                CourseHeader('ME 213: THERMOSCIENCES I (2 UNITS)'),
-                CourseContent(
+                const CourseHeader('ME 213: THERMOSCIENCES I (2 UNITS)'),
+                const CourseContent(
                   content:
                       'Introduction to Thermodynamics; Parameters of state and units; Equations of state for ideal and real gases; specific heat; The first law of thermodynamics for steady and unsteady processes; Calorific functions of state; Internal energy. Enthalpy, chemical energy (calorific value and heat of combustion). Work and power in thermodynamic system; Basic Thermodynamic processes; Thermodynamic cycles, Efficiency of the cycle, Carnot cycle; the second law of thermodynamics; Entropy.',
                 ),
-                CourseHeader('ME 214: WORKSHOP PRACTICE I (1 UNITS)'),
-                CourseContent(
+                const CourseHeader('ME 214: WORKSHOP PRACTICE I (1 UNITS)'),
+                const CourseContent(
                   content:
                       'Industrial Safety: Environmental factors, clothing and footwear, eye protection, mechanical hazards, chemical hazards, electrical accidents, fire prevention. Use of hand tools, simple marking including the use of special instruments (surface gauge, combination set, etc). Measuring instruments (Micrometers, feeler gauges, thread gauges, etc). Simple machining (drilling, countersinking, spot facing, reaming and boring). Screw threads (thread forms, taps and dies, thread tapping, thread cutting on lathe). Joining processes, brazing, riveting, Sheet metal forming, nuts and bolts. Inserted threads, self-tapping screws. Woodwork: Use of woodworking tools, planning, joints. Use of wood turning lathe.',
                 ),
-                CourseHeader('MTH 212: MATHEMATICAL METHODS 1 (3 UNITS)'),
-                CourseContent(
+                const CourseHeader('MTH 212: MATHEMATICAL METHODS 1 (3 UNITS)'),
+                const CourseContent(
                   title: 'Ordinary Differential Equations:',
                   content:
                       'First and second order differential equations. Existence and uniqueness. Methods of undetermined coefficients. Method of variation of parameters, series solution of second order equations. The general theory of nth order linear equations. Application of O.D.E. to Physical, life and social sciences.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title: 'Vectors:',
                   content:
                       'Dot, cross and triple products, vector field theory, divergence, gradient and curl. Expressions for grad, div and curl in orthogonal, curvilinear coordinates.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title: 'Multiple Integrals:',
                   content:
                       'Line integrals, multiple integrals change of variables, the Jacobian. Area of surface and volume of a solid.',
                 ),
-                CourseHeader('GNS 311: BUSINESS CREATION AND GROWTH (2 UNITS)'),
-                CourseContent(
+                const CourseHeader('GNS 311: BUSINESS CREATION AND GROWTH (2 UNITS)'),
+                const CourseContent(
                   title: 'History of Science and Technology Man:',
                   content: 'His origin and nature and his cosmic environment.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title: 'Advances in Technology:',
                   content:
                       'From stone Metal: Greco Roman/Medieval Technology, Internal Combustion/Industrial Revolution and Miniaturization.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title: 'Philosophy of Science and Technology:',
                   content:
                       'Aims, nature and functions of science including scientific theories, laws and explanations, scientific methodology, science and morality. Science and Technology in the society and in the service of man.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title: 'Renewable and Non-Renewable Resources:',
                   content:
                       'Man and his energy resources. Environmental Effects of Chemicals, Plastics, textiles and other materials. Chemical and radio-chemical hazards.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title:
                       'A review of 20th Century Scientific Applications and Technological Explosion:',
                   content:
                       'Post World War II Scientific and Technological Advancement, Scientific innovations in the developed world - U.S., Germany, U.K., France, USSR, Scientific innovations in the developing world - India, China, Brazil and Science and technology in the third world. The Development of Science and Technology in Nigeria:- History and Structural Organization of Research Centres in Nigeria. Research Policy in Nigeria, the challenge of scientific and technological innovation in Nigeria, the appropriate science and technology for a developing society and Ministry of Science and Technology aims and functions.',
                 ),
-                CourseContent(
+                const CourseContent(
                   title: 'The future of Science and Technology:',
                   content:
                       'Automation; Man versus machine, Nuclear stockpiles, arms race, genetic manipulation and problems of the developed world and their likely impact on the developing nations.',
                 ),
-                CourseHeader('EG 210: ENGINEERING LABORATORIES I (3 UNITS)'),
-                CourseContent(
+                const CourseHeader('EG 210: ENGINEERING LABORATORIES I (3 UNITS)'),
+                const CourseContent(
                     content:
                         'A laboratory on 200-Level Engineering courses designed to illustrate topics covered in Electrical, Mechanical and Civil Engineering Courses at First Semester.'),
-                CourseHeader('ES 217: ENGINEER IN SOCIETY (2 UNITS)'),
-                CourseContent(
+                const CourseHeader('ES 217: ENGINEER IN SOCIETY (2 UNITS)'),
+                const CourseContent(
                     content:
                         'Philosophy of Science.  History of Engineering and Technology .Safety in Engineering and Introduction to Risk Analysis.The Role of Engineers in Nation Building. Invited Lectures from Professionals')
                 ,
                 Padding(
-                  padding: EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(8.0),
                   child: SizedBox(
                     height: MediaQuery.of(context).size.height - 400,
                     child: WebViewWidget(controller: webViewController),
@@ -146,5 +146,5 @@ class SecondYearFirstSemesterState extends State<SecondYearFirstSemester>{
 Future<void> _onLoadFlutterAsset(
     WebViewController controller,
     BuildContext context) async {
-  await controller.loadFlutterAsset('assets/tables/hundred_level_fs.html');
+  await controller.loadFlutterAsset('assets/tables/two_hundred_fs.html');
 }
--- lib/pages/card_destinations/third_year_first_semester_screen.dart
+++ lib/pages/card_destinations/third_year_first_semester_screen.dart
@@ -1,11 +1,115 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import '../../widgets/course_contents.dart';
+import '../../widgets/course_header.dart';
 
-class ThirdYearFirstSemester extends StatelessWidget {
+class ThirdYearFirstSemester extends StatefulWidget {
   const ThirdYearFirstSemester({super.key, required this.title});
+
   final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  State<StatefulWidget> createState() => ThirdYearFirstSemesterState();
+}
+
+class ThirdYearFirstSemesterState extends State<ThirdYearFirstSemester> {
+  late final WebViewController webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    webViewController = WebViewController();
+    _onLoadFlutterAsset(webViewController, context);
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CourseHeader(
+                'MTH 337: NUMERICAL ANALYSIS I (3 UNITS)',
+              ),
+              const CourseContent(
+                content:
+                'Solution of single algebraic equations: Newton\'s method, Bairstow\'s method. Interpolation. Solution of Systems of equations: Matrix algebra, Gaussian methods, LU decomposition, iterative methods, matrix inversion. Interpolating polynomials. Numerical differentiation and integration. Numerical solutions of ordinary differential equations: Initial and boundary value problems. Euler\'s method, Taylor series and method, Runge-Kutta, predictor-corrector methods, multi-step methods. Systems of equations and higher order equations. Finite difference calculus: Difference equations.',
+              ),
+              const CourseHeader(
+                'EA 311: INTRODUCTORY ENGINEERING STATISTICS (3 UNITS)',
+              ),
+              const CourseContent(
+                content:
+                'Measures of central tendency and dispersion, (grouped and ungrouped): mean - arithmetic, geometric, Harmonic; median; mode; quartiles, deciles and percentiles. Empirical relation between mean, median and mode. Relative and absolute dispersion. Sample space and events as sets. Finite probability space. Properties of probability. Statistical independence and conditional probability. Tree diagram. Bayes theorem. Discrete and continuous random variables. Expectation. Independent Bernoulli trails. Binomial, Poisson and Normal Distributions. Normal approximation to Binomial and Poisson distributions. Hypergeometric.',
+              ),
+              const CourseHeader(
+                'EE 311: CIRCUIT THEORY I (3 UNITS)',
+              ),
+              const CourseContent(
+                content:
+                'Laplace and Fourier transforms, application of Laplace transformation to transient analysis of RLC circuits, transfer function concept, realizability of transfer functions, Foster and Cauer\'s methods of synthesis 2-port network synthesis, active filters. Analysis of continuous and discrete signals and systems.',
+              ),
+              const CourseHeader(
+                'EE 312: ANALOGUE ELECTRONIC CIRCUITS (3 UNITS)',
+              ),
+              const CourseContent(
+                content:
+                'Review of single stage transistor amplifiers and operational amplifier circuits. Analysis and design of multistage amplifiers. Feedback, Broadband and Narrow band amplifiers, power amplifiers, voltage and current stabilizing circuits. Sinusoidal RC and LC and crystal oscillators, other communication circuits.',
+              ),
+              const CourseHeader(
+                'EE 313: ELECTROMAGNETIC FIELDS AND WAVES I (3 UNITS)',
+              ),
+              const CourseContent(
+                content:
+                'Review of electromagnetic laws in integral form, Gauss\'s Law, Ampere\'s and Faraday\'s Laws; Electrostatic fields due to distribution of charge magnetic fields in and around current carrying conductors; Introduction to time varying magnetic and electric fields; conduction and displacement current, Maxwell\'s equations (in rectangular coordinates and vector calculus notations.)',
+              ),
+              const CourseHeader(
+                'EE 314: TELECOMMUNICATION PRINCIPLES (3 UNITS)',
+              ),
+              const CourseContent(
+                content:
+                'Amplitude modulation; double sideband, single sideband, and vestigial sideband modulation schemes; simple modulators, power and bandwidth performance. Angle modulation: frequency modulation, phase modulation, bandwidth requirement, clippers and limiters. Amplitude modulated signal reception: Demodulation techniques, detectors, and noise performance. Angle modulated signal reception: discrimination frequency tracking loop, phase locked loop and noise performance. Commercial radio system. Transmission media; attenuation in open space, air, cable and fibre channels; construction of cables and fibres. Sampling theorem, pulse amplitude modulation, pulse code modulation, delta modulation, sources and correction of errors in PCM and DM, ideal and matched filters, frequency acquisition, phase referencing and timing. Line codes, block encoding and Shannon\'s theorem.',
+              ),
+              const CourseHeader(
+                'EE 315: MEASUREMENTS AND INSTRUMENTATION (3 UNITS)',
+              ),
+              const CourseContent(
+                content:
+                'General instrumentation, Basic Meter in DC Measurements, Basic Meter in AC Measurements, rectifier voltmeter, electrodynamometer and wattmeter, instrument transformers; DC and AC bridges and their applications: general form of AC bridge, universal impedance bridge; Electronic Instruments for the Measurement of voltage, current, resistance and other circuit parameter: electronic voltmeters, AC voltmeters using rectifiers, electronic multimeter, digital voltmeters; oscilloscope: vertical deflection system, horizontal deflection system, probes, sampling CRO; Instruments for generating and analysing waveforms; square-wave and pulse generator, signal generators, function generators, wave analyzers; Electronic counters and their applications; time base circuitry, universal counter measurement modes; Analogue and digital data acquisition systems; tape recorders, D/A and A/D conversions, sample and hold circuits.',
+              ),
+              const CourseHeader(
+                'EE 316: TELECOMMUNICATIONS LABORATORY (1 UNIT)',
+              ),
+              const CourseContent(
+                content:
+                'A laboratory work on telecommunication principles and designed to illustrate topics covered in EE 314',
+              ),
+              const CourseHeader(
+                'EE 317: ELECTRICAL AND ELECTRONICS WORKSHOP (1 UNIT)',
+              ),
+              const CourseContent(
+                content:
+                'Passive and active components; service accessories and ratings; IC nomenclature, data sheets and application; Electrical and Electronics symbols; Use of testing and measuring instruments; safety engineering; workshops practice; Installation practice, prototyping and soldering Trouble-shooting; Introduction to manufacturing technology. Illustrative project assignments.',
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height - 400,
+                  child: WebViewWidget(controller: webViewController),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Future<void> _onLoadFlutterAsset(
+    WebViewController controller, BuildContext context) async {
+  await controller.loadFlutterAsset('assets/tables/three_hundred_fs.html');
 }
