f42b60b211b0720792ea744fc9da999feba60502
Great Grant <greatgrant116@gmail.com>
2024-01-21 21:25:13 +0100

    handle exit app with a dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:dcce_handbook/pages/home_page.dart';
 import 'package:dcce_handbook/util/theme_provider.dart';
-import 'package:dcce_handbook/widgets/common_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:dcce_handbook/util/DrawerSelection.dart';
 import 'package:dcce_handbook/widgets/grid_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../models/level.dart';
 import '../widgets/common_app_bar.dart';
 import '../widgets/app_drawer.dart';
@@ -22,51 +23,99 @@ class HomePage extends StatefulWidget{
 
 class HomePageState extends State<HomePage> {
 
+  void _showBackDialog() {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Are you sure?'),
+          content: const Text(
+            'Are you sure you want to leave this page?',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Nevermind'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Leave'),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pop(context);
+
+                // Exit app
+                SystemNavigator.pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: CommonAppBar(title: widget.title),
-        drawer: const AppDrawer(initialSelection: DrawerSelection.home),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text("Academic Levels",
-                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      fontFamily: "montserrat"
-                  )
-              ),
-              const SizedBox(height: 8,),
-              Text("Select A Level Below To See Its Syllabus",
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      fontFamily: "montserrat"
-                  ),
-              ),
-              const SizedBox(height: 8,),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 4,
-                    crossAxisSpacing: 4,
-                  ),
-                  itemCount: Level.levels.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final currentLevel = Level.levels[index];
-                    return GestureDetector(
-                      onTap: () {
-                        _navigateToDestination(context, index);
-                      },
-                      child: buildGridItem(currentLevel),
-                    );
-                  },
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didPop) {
+        if (didPop) {
+          return;
+        }
+        _showBackDialog();
+      },
+      child: Scaffold(
+          appBar: CommonAppBar(title: widget.title),
+          drawer: const AppDrawer(initialSelection: DrawerSelection.home),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Academic Levels",
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        fontFamily: "montserrat"
+                    )
                 ),
-              )
-            ],
-          ),
-        )
+                const SizedBox(height: 8,),
+                Text("Select A Level Below To See Its Syllabus",
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        fontFamily: "montserrat"
+                    ),
+                ),
+                const SizedBox(height: 8,),
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                    ),
+                    itemCount: Level.levels.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final currentLevel = Level.levels[index];
+                      return GestureDetector(
+                        onTap: () {
+                          _navigateToDestination(context, index);
+                        },
+                        child: buildGridItem(currentLevel),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
+      ),
     );
   }
 
@@ -92,4 +141,5 @@ void _navigateToDestination(BuildContext context, int index){
     default:
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const CurriculumScreen(title: 'Syllabus',)));
   }
+
 }
