bbe5f4cb56b1c131e26985be148ff2a87450a2cd
Great Grant <greatgrant116@gmail.com>
2024-01-24 06:39:55 +0100

    adjust back dialog font family

--- lib/widgets/show_back_dialog.dart
+++ lib/widgets/show_back_dialog.dart
@@ -7,16 +7,17 @@ void showBackDialog(BuildContext context) {
     context: context,
     builder: (context) {
       return AlertDialog(
-        title: const Text('Are you sure?'),
+        title: const Text('Are you sure?', style: TextStyle(fontFamily: "montserrat"),),
         content: const Text(
           'Are you sure you want to leave this page?',
+          style: TextStyle(fontFamily: "montserrat"),
         ),
         actions: <Widget>[
           TextButton(
             style: TextButton.styleFrom(
               textStyle: Theme.of(context).textTheme.labelLarge,
             ),
-            child: const Text('Nevermind'),
+            child: const Text('Nevermind', style: TextStyle(fontFamily: "montserrat"),),
             onPressed: () {
               Navigator.pop(context);
             },
@@ -25,7 +26,7 @@ void showBackDialog(BuildContext context) {
             style: TextButton.styleFrom(
               textStyle: Theme.of(context).textTheme.labelLarge,
             ),
-            child: const Text('Leave'),
+            child: const Text('Leave', style: TextStyle(fontFamily: "montserrat"),),
             onPressed: () {
               Navigator.pop(context);
 
