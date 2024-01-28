e49e387656793c5157e44863e5702d14ca54019e
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:36:26 +0100

    add documentation for showBackDialog

--- lib/widgets/show_back_dialog.dart
+++ lib/widgets/show_back_dialog.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
+/// Shows a confirmation dialog when attempting to navigate back.
+///
+/// The [showBackDialog] function displays a confirmation dialog when the user
+/// attempts to navigate back from the current page. It prompts the user with
+/// the question "Are you sure you want to leave this page?" and provides two
+/// options: "Nevermind" to cancel the action and remain on the page, and "Leave"
+/// to confirm the action and exit the application.
+///
+/// - [context]: The [BuildContext] for the widget tree.
 void showBackDialog(BuildContext context) {
   showDialog<void>(
     context: context,
