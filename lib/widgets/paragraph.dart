7e3272a4315516c1914b2302d7dde34b9b7f06a7
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:35:20 +0100

    add documentation for build paragraph

--- lib/widgets/paragraph.dart
+++ lib/widgets/paragraph.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/// Builds a paragraph widget with customizable text style.
+///
+/// The [buildParagraph] function is used to create a paragraph widget
+/// with the specified text and text style. By default, the text is styled
+/// based on the current theme, but the color can be overridden if the text
+/// represents a penalty or error.
+///
+/// - [context]: The [BuildContext] for the widget tree.
+/// - [text]: The text content of the paragraph.
+/// - [isPenalty]: Flag indicating whether the text represents a penalty or error.
 Widget buildParagraph(BuildContext context, String text, {bool isPenalty = false}) {
   return Padding(
     padding: const EdgeInsets.only(bottom: 8.0),
