b69fd59c6ad61eaaa45ca6f5cfb8c1d74184b04a
Great Grant <greatgrant116@gmail.com>
2024-01-28 23:42:00 +0100

    add documentation for build drawer

--- lib/widgets/build_header.dart
+++ lib/widgets/build_header.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/// Builds a heading widget with the specified [text].
+///
 Widget buildHeading(String text) {
   return Padding(
     padding: const EdgeInsets.symmetric(vertical: 8.0),
