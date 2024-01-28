c23a960c99facdc3b56033fd62d211cf4eefa5aa
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:34:45 +0100

    add documentation for list tile icon

--- lib/widgets/list_tile_icon.dart
+++ lib/widgets/list_tile_icon.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
-
 import '../util/drawer_selection.dart';
 
+/// A widget for displaying an icon in a list tile, with dynamic color based on selection.
+///
+/// The [ListTileIcon] class is used to display an [Icon] within a list tile,
+/// with the color of the icon changing based on whether the corresponding drawer
+/// item is currently selected or not.
 class ListTileIcon extends StatelessWidget {
+  /// Creates a [ListTileIcon] widget.
+  ///
+  /// The [icon] parameter specifies the icon to be displayed.
+  /// The [drawerSelection] parameter indicates the selection status of the drawer item.
+  /// The [currentSelection] parameter indicates the current selected drawer item.
   const ListTileIcon({
     super.key,
     required this.icon,
@@ -10,8 +19,13 @@ class ListTileIcon extends StatelessWidget {
     required this.currentSelection,
   });
 
+  /// The icon to be displayed in the list tile.
   final IconData icon;
+
+  /// The selection status of the drawer item associated with the icon.
   final DrawerSelection drawerSelection;
+
+  /// The currently selected drawer item.
   final DrawerSelection currentSelection;
 
   @override
