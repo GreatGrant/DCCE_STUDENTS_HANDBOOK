23812f477c319b2eabc9e0e78de84fd8768b7024
Great Grant <greatgrant116@gmail.com>
2024-01-28 23:52:23 +0100

    add documentation for CommonDropCap

--- lib/widgets/common_dropcap.dart
+++ lib/widgets/common_dropcap.dart
@@ -2,7 +2,16 @@ import 'package:drop_cap_text/drop_cap_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/// A common widget for displaying text with a drop cap.
+///
+/// The [CommonDropCap] widget is used to display text with a drop cap,
+/// which is a large initial letter at the beginning of a paragraph.
+/// This widget provides a consistent style for drop cap text across the app.
 class CommonDropCap extends StatelessWidget {
+  /// Constructs a [CommonDropCap] with the specified [paragraph].
+  ///
+  /// The [paragraph] parameter is required and specifies the text content
+  /// of the paragraph to be displayed with a drop cap.
   const CommonDropCap({
     super.key,
     required this.paragraph,
