9d5388cbd7b46cc5273b77a2b7e6a883908fb453
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:08:10 +0100

    add documentation for CourseContent

--- lib/widgets/course_contents.dart
+++ lib/widgets/course_contents.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/// A widget representing a course content with optional title.
+///
+/// The [CourseContent] widget is used to display course content with an optional title.
+/// It consists of a column containing a title (if provided) and the content text.
 class CourseContent extends StatelessWidget {
+  /// The title of the course content. If not provided, an empty string is used.
   final String title;
+
+  /// The main content of the course.
   final String content;
+
+  /// Constructs a [CourseContent] with the specified [title] and [content].
+  ///
+  /// The [title] parameter is optional and represents the title of the course content.
+  /// The [content] parameter is required and represents the main content of the course.
 
   const CourseContent({this.title = "", required this.content, super.key} );
 
