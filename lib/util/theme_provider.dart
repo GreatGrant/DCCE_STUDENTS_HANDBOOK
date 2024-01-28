e148df3898c867d24ece641f01c0dab9f1ce5a75
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:41:58 +0100

    add documentation for ThemeProvider

--- lib/util/theme_provider.dart
+++ lib/util/theme_provider.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
-
+/// A class that provides the theme mode for the app and notifies listeners when the theme mode changes.
 class ThemeProvider with ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.light;
+
+  /// Gets the current theme mode.
   ThemeMode get themeMode => _themeMode;
 
+  /// Sets the theme mode and notifies listeners.
+  ///
+  /// The [themeMode] parameter specifies the new theme mode to be set.
   void setThemeMode(ThemeMode themeMode){
     _themeMode = themeMode;
     notifyListeners();
