d9ff26f7539576fc087a0ff96c78d0c63a81663c
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:06:32 +0100

    add documentation for CommonYearScreen

--- lib/widgets/common_year_screen.dart
+++ lib/widgets/common_year_screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'common_app_bar.dart';
 
+/// A common screen widget for displaying content organized by academic year with tabs.
+///
+/// The [CommonYearScreen] widget is used to create a common screen layout
+/// for displaying content organized by year with tabs. It consists of a scaffold
+/// with an app bar containing the title and tabs, and a tab bar view displaying
+/// the content associated with each tab.
 class CommonYearScreen extends StatelessWidget {
+  /// Constructs a [CommonYearScreen] with the specified parameters.
+  ///
+  /// The [title] parameter is required and represents the title of the screen.
+  /// The [tabs] parameter is required and represents the tabs to be displayed
+  /// in the app bar. The [tabViewChildren] parameter is required and represents
+  /// the content associated with each tab.
+
   const CommonYearScreen({
     super.key,
     required this.title,
@@ -9,8 +22,13 @@ class CommonYearScreen extends StatelessWidget {
     required this.tabViewChildren,
   });
 
+  /// The title of the screen.
   final String title;
+
+  /// The tabs to be displayed in the app bar.
   final List<Widget> tabs;
+
+  /// The content associated with each tab.
   final List<Widget> tabViewChildren;
 
   @override
