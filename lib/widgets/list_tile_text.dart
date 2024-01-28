33607b628fdcc0500fba3307dad533a6499c31c5
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:35:02 +0100

    add documentation for list tile text

--- lib/widgets/list_tile_text.dart
+++ lib/widgets/list_tile_text.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import '../util/drawer_selection.dart';
 
+/// A widget for displaying text in a list tile, with dynamic style based on selection.
+///
+/// The [ListTileText] class is used to display text within a list tile,
+/// with the style of the text changing based on whether the corresponding drawer
+/// item is currently selected or not.
 class ListTileText extends StatelessWidget {
+  /// Creates a [ListTileText] widget.
+  ///
+  /// The [text] parameter specifies the text to be displayed.
+  /// The [drawerSelection] parameter indicates the selection status of the drawer item.
+  /// The [currentSelection] parameter indicates the current selected drawer item.
   const ListTileText({
     super.key,
     required this.text,
@@ -11,8 +20,13 @@ class ListTileText extends StatelessWidget {
     required this.currentSelection,
   });
 
+  /// The text to be displayed in the list tile.
   final String text;
+
+  /// The selection status of the drawer item associated with the text.
   final DrawerSelection drawerSelection;
+
+  /// The currently selected drawer item.
   final DrawerSelection currentSelection;
 
   @override
