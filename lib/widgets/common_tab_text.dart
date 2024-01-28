0aa0778b778278570094a540723bb17c777cc9d6
Great Grant <greatgrant116@gmail.com>
2024-01-29 00:02:51 +0100

    add documentation for CommonTabText

--- lib/widgets/common_tab_text.dart
+++ lib/widgets/common_tab_text.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/// A common widget for displaying text in tabs.
+///
+/// The [CommonTabText] widget is used to create a common style for text
+/// displayed in tabs. It applies a bold Montserrat font style to the text
+/// for consistent tab text styling across the app.
 class CommonTabText extends StatelessWidget {
+  /// Constructs a [CommonTabText] with the specified [text].
+  ///
+  /// The [text] parameter is required and represents the text content
+  /// to be displayed in the tab.
   const CommonTabText({
     super.key,
     required this.text
   });
 
+  /// The text content to be displayed in the tab.
   final String text;
 
   @override
