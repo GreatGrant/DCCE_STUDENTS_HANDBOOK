fd54d134c5b197617446268eedd66216ae162608
Great Grant <greatgrant116@gmail.com>
2024-01-30 07:49:24 +0100

    add google font asset destination in pubspec

--- lib/util/webview_utils.dart
+++ lib/util/webview_utils.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+/// A utility class for working with web views.
 class WebViewUtils {
+  /// Loads HTML content into a web view with specified background color and text color.
+  ///
+  /// The [controller] parameter is the web view controller.
+  ///
+  /// The [htmlContent] parameter is the HTML content to load into the web view.
+  ///
+  /// The [backgroundColor] parameter is the background color of the web view.
+  ///
+  /// The [isColorDark] parameter specifies whether the background color is dark or light.
+  ///
+  /// This method sets the background color of the web view, modifies text color and table header color based on the darkness of the background color.
   static Future<void> loadHtmlContent({
     required WebViewController controller,
     required String htmlContent,
@@ -11,8 +25,9 @@ class WebViewUtils {
     // Load HTML content into the WebView
     await controller.loadHtmlString(htmlContent);
 
+    // Clear cache to ensure styles are applied
+    controller.clearCache();
     // Set background color after the web page has finished loading
-    controller.clearCache(); // Clear cache to ensure styles are applied
     controller.setBackgroundColor(backgroundColor);
     controller.setJavaScriptMode(JavaScriptMode.unrestricted);
 
@@ -27,6 +42,11 @@ class WebViewUtils {
     ''');
   }
 
+  /// Determines whether a given color is dark or light.
+  ///
+  /// The [color] parameter is the color to be evaluated.
+  ///
+  /// Returns true if the color is considered dark, and false otherwise.
   static bool isColorDark(Color color) {
     // Calculate the luminance of the color
     final luminance = color.computeLuminance();
