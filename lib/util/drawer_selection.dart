9040c89f8851c65508d43d832b1e76cf3c6833f7
Great Grant <greatgrant116@gmail.com>
2024-01-30 07:30:17 +0100

    add documentation to drawer selection

--- lib/util/drawer_selection.dart
+++ lib/util/drawer_selection.dart
@@ -1,3 +1,29 @@
-enum DrawerSelection{
-  home, history, philosophy, examGuidelines, academicRegulation, admissionReq, courseDuration, grading, staff
+/// Enumeration representing different selections for the app drawer.
+enum DrawerSelection {
+  /// Represents the selection for the home screen.
+  home,
+
+  /// Represents the selection for the history screen.
+  history,
+
+  /// Represents the selection for the philosophy screen.
+  philosophy,
+
+  /// Represents the selection for the exam guidelines screen.
+  examGuidelines,
+
+  /// Represents the selection for the academic regulation screen.
+  academicRegulation,
+
+  /// Represents the selection for the admission requirements screen.
+  admissionReq,
+
+  /// Represents the selection for the course duration screen.
+  courseDuration,
+
+  /// Represents the selection for the grading screen.
+  grading,
+
+  /// Represents the selection for the staff screen.
+  staff,
 }
